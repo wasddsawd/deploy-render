ed22cf3a84d175d116a65811684d5c15a403f7aa
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-09-03 15:35:19 -0300

    add path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
